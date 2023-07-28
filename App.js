7d87824418dcaeca9542084cceeaead91cd83b1f
Diana Smilian <dianka.kulakevich@gmail.com>
2023-07-28 20:09:59 +0300

    finish learn style

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  Button,
+  Alert,
+  ImageBackground,
+  View,
+} from 'react-native';
 
 export default function App() {
+  const handlePressButton = () => {
+    return Alert.alert('Hello!', 'Say hello me)', [
+      { text: 'Hello', onPress: handlePressHello },
+      { text: 'Exit' },
+    ]);
+  };
+
+  const handlePressHello = () =>
+    Alert.alert('Hello!', 'Thanks, have a nice day😉', [{ text: 'Thanks' }, ,]);
+
   return (
     <View style={styles.container}>
-      <Text>Hello word!</Text>
-      <Button title="Click me" />
+      <ImageBackground
+        source={require('./assets/background.png')}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Text>Hello word!</Text>
+        <Button title="Click" onPress={handlePressButton} />
+        <Text>Vay, this is good!</Text>
+      </ImageBackground>
 
       <StatusBar style="auto" />
     </View>
@@ -16,6 +40,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
   },
 });
