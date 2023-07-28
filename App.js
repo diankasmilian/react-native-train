4fb0eff4e82dd2f16d0d009719594bb462767f9b
Diana Smilian <dianka.kulakevich@gmail.com>
2023-07-28 18:16:03 +0300

    error

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello word!</Text>
+      <Button title="Click me" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +17,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
